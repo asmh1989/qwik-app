beebbc72d33fca9ce98cdaecf9d15479bcd008b4
sun <asmh1989@gmail.com>
2024-01-29 11:45:36 +0800

    init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,23 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, $ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { invoke } from '@tauri-apps/api/tauri'
 
 export default component$(() => {
+  const greetMsg = useSignal('')
+
+  const greet = $(async (name: string) => {
+    greetMsg.value = await invoke('greet', { name })
+  })
   return (
     <>
       <h1>Hi 👋</h1>
       <p>
         Can't wait to see what you build with qwik!
         <br />
-        Happy coding.
+        Happy coding ....
       </p>
+      <button onClick$={() => greet(new Date().toTimeString())}>{greetMsg.value}</button>
+
     </>
   );
 });
